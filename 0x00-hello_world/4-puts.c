f685e1f42399aa585af4ec631aeaeb5fad3708cb
Stephen Kahohi <stevekahohi1@gmail.com>
2023-09-07 13:05:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
- /**
+/**
  * main - A line that prints puts function
  * Return: Always 0 (Success)
-  */
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
