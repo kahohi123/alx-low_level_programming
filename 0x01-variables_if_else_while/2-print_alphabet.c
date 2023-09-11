51c777b4fa6fc8f7cf80c194df1b2d159d5995d3
Stephen Kahohi <stevekahohi1@gmail.com>
2023-09-11 14:43:03 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
