a2aa420e71d15b18cc11205f24f03055a02df66a
Stephen Kahohi <stevekahohi1@gmail.com>
2023-09-11 17:48:45 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,25 +8,30 @@
 int main(void)
 {
 	int i = '0';
-	int j;
+	int j = '0';
 
 	while (i <= '9')
 	{
-		j = '0';
-		while (j < '9')
+		while (j <= '9')
 		{
-		if (i < j)
+			if (!(i > j) || i == j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
 		}
-			putchar(i);
-			putchar(j);
-			if (i != '8' || (i == '8' && j != '9'))
-			putchar(',');
-			putchar(' ');
-		}
-		j++
-		}
-		i++
+		j = '0';
+		i++;
 	}
-	putchar('\n');
 	return (0);
 }
