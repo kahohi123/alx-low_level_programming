c519f48f674082eddaa20d4a2ab5b09ddefe74b7
Stephen Kahohi <stevekahohi1@gmail.com>
2023-09-12 16:41:30 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabets x10 times
+ * Return: void
+ */
+
+void pint_alphabet_x10(void)
+{
+
+char ch;
+int i;
+
+i = 0;
+while (i < 10)
+{
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+i++;
+}
+}
